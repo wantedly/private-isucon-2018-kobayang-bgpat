c87a2c05b71573600195bda8cbec85c7924e89b8
bgpat <bgpat@users.noreply.github.com>
2018-05-01 14:04:10 +0900

    Remove comment

--- go/product.go
+++ go/product.go
@@ -55,38 +55,12 @@ func getProductsWithCommentsAt(page int) []ProductWithComments {
 		err = rows.Scan(&p.ID, &p.Name, &p.Description, &p.ImagePath, &p.Price, &p.CreatedAt)
 
 		// select comment count for the product
-		/*
-			var cnt int
-			cnterr := db.QueryRow("SELECT count(*) as count FROM comments WHERE product_id = ?", p.ID).Scan(&cnt)
-			if cnterr != nil {
-				cnt = 0
-			}
-			p.CommentCount = cnt
-		*/
 		cnt := len(productComments[p.ID])
 		p.CommentCount = cnt
 
 		if cnt > 0 {
 			// select 5 comments and its writer for the product
 			var cWriters []CommentWriter
-
-			/*
-				subrows, suberr := db.Query("SELECT * FROM comments as c INNER JOIN users as u "+
-					"ON c.user_id = u.id WHERE c.product_id = ? ORDER BY c.created_at DESC LIMIT 5", p.ID)
-				if suberr != nil {
-					subrows = nil
-				}
-
-				defer subrows.Close()
-				for subrows.Next() {
-					var i int
-					var s string
-					var cw CommentWriter
-					subrows.Scan(&i, &i, &i, &cw.Content, &s, &i, &cw.Writer, &s, &s, &s)
-					cWriters = append(cWriters, cw)
-				}
-			*/
-
 			for i := 0; i < 5; i++ {
 				if i >= cnt {
 					break
