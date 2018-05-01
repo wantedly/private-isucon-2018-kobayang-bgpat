881c0221a847990ac211e2620afd38d8e0b7688e
kobayang <naokiod@gmail.com>
2018-05-01 16:38:22 +0900

    Loading in main func

--- go/initialize.go
+++ go/initialize.go
@@ -12,10 +12,14 @@ func initialize(c *gin.Context) {
 	db.Exec("DELETE FROM comments WHERE id > 200000")
 	db.Exec("DELETE FROM histories WHERE id > 500000")
 
+	loading()
+
+	c.String(http.StatusOK, "Finish")
+}
+
+func loading() {
 	loadComments()
 	loadUsers()
 	loadProducts()
 	loadHistories()
-
-	c.String(http.StatusOK, "Finish")
 }
