b0e518645dd353df6c6552bc800e5180d62aa786
bgpat <bgpat@users.noreply.github.com>
2018-05-01 12:48:32 +0900

    Extract initialize

---
+++ go/initialize.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func initialize(c *gin.Context) {
+	db.Exec("DELETE FROM users WHERE id > 5000")
+	db.Exec("DELETE FROM products WHERE id > 10000")
+	db.Exec("DELETE FROM comments WHERE id > 200000")
+	db.Exec("DELETE FROM histories WHERE id > 500000")
+
+	c.String(http.StatusOK, "Finish")
+}
--- go/main.go
+++ go/main.go
@@ -212,12 +212,7 @@ func main() {
 
 	// GET /initialize
 	r.GET("/initialize", func(c *gin.Context) {
-		db.Exec("DELETE FROM users WHERE id > 5000")
-		db.Exec("DELETE FROM products WHERE id > 10000")
-		db.Exec("DELETE FROM comments WHERE id > 200000")
-		db.Exec("DELETE FROM histories WHERE id > 500000")
-
-		c.String(http.StatusOK, "Finish")
+		initialize(c)
 	})
 
 	r.Run(":8080")
